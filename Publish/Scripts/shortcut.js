b39a5883149f849b9564d849a65b35b3e8102423
Asiri Dissanayaka <asiridissanayaka@gmail.com>
2017-10-05 23:31:39 +0530

    1.0.12.218

---
+++ Publish/Scripts/shortcut.js
@@ -0,0 +1,38 @@
+﻿shortcut = {
+    keys: new Object(),
+    add: function (array, func) {
+        var hey = this;
+        var counter = this.stingify(array);
+        this.keys[counter] = func;
+        var collect = new Array();
+        document.body.addEventListener('keydown', function (e) {
+            if (e.keyCode === array[collect.length]) {
+                collect[collect.length] = e.keyCode;
+            } else {
+                collect = new Array();
+            }
+            if (collect.length === array.length) {
+                e.stopPropagation();
+                collect = new Array();
+                hey.keys[counter]();
+            }
+        }, false);
+
+    },
+    remove: function (array) {
+        this.keys[this.stingify(array)] = function () {
+
+        };
+    },
+    stingify: function (array) {
+        var counter = '';
+        for (x in array) {
+            if (x == 0) {
+                counter += array[x];
+            } else {
+                counter += '+' + array[x];
+            }
+        }
+        return counter;
+    }
+};
